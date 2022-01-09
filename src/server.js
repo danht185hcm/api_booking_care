e8394524108a0d01dfe2def2da5b7f4334e7d113
Danh Tran <danht185hcm@gmail.com>
2022-01-09 22:51:34 +0700

    connect database mysql width sequelize

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import bodyParser from 'body-parser';
 import express from 'express';
+import connectDB from './config/connectDB';
 import configViewEngine from './config/viewEngine';
 import route from './routes';
 
@@ -10,6 +11,7 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+connectDB();
 configViewEngine(app);
 route(app);
 
