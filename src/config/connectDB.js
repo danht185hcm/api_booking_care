60e797f5b15d2f19f60623b92cd1912447fe4ad0
Danh Tran <danht185hcm@gmail.com>
2022-01-13 18:36:00 +0700

    [hot fixed]: Một số thay đổi nhỏ

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -1,6 +1,6 @@
 import { Sequelize } from 'sequelize';
 
-const sequelize = new Sequelize('hoidanit', 'root', null, {
+const sequelize = new Sequelize('booking_care_dev', 'root', null, {
   host: 'localhost',
   dialect: 'mysql',
   logging: false,
